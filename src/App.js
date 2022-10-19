03cbee4a4914ab82ddf75f17b54af1e05b1f600a
paulmcgonigle67 <s00210326@atu.ie>
2022-10-19 11:24:24 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import './App.css'
+
 
 function App() {
+  
+  const apiKey = '426b8ee7a7244bbb2d02fd169da663f1'
+  const[weatherData,setWeatherData] = useState([{}])
+  const[city,setCity] = useState("")
+
+  const getWeather = (event)=>{
+    if(event.key == "Enter"){
+      fetch('https://api.openweathermap.org/data/3.0/weather?q=${city}&appid=${apiKey}').then(
+        response => response.json()
+      ).then(
+        data => {
+          setWeatherData(data)
+        }
+      )
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+<input className='input'
+ placeholder='Enter Beach...'
+ onChange={e => setCity(e.target.value)}
+ value={city}
+ onKeyPress={getWeather}
+ >
+  
+ </input>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
